415bbe0093b95e3a3c44818492cc14212711c617
bhavana-krnm <bhavanakrnm@gmail.com>
2026-06-12 14:22:18 +0530

    css layouts

---
+++ Input.js
@@ -0,0 +1,17 @@
+function Another_name()
+{
+    let text=document.getElementById("name");
+    text.innerHTML="Chaithu";
+    let bhavana=document.getElementById("bhav");
+    bhavana.src="girl2.jpg";
+}
+
+function Change_color()
+{
+    let colour=document.getElementById("cap");
+    colour.style.backgroundColor="green";
+    let txt1=document.getElementById("data");
+    txt1.style.color="white";
+    let txt2=document.getElementById("name");
+    txt2.style.color="white";
+}
--- manipulation.js
+++ manipulation.js
@@ -1,4 +1,4 @@
-let pen=document.getElementById("wish");
+/*let pen=document.getElementById("wish");
 console.log(pen);
 let mouse=document.getElementsByClassName("wishes");
 console.log(mouse);
@@ -7,4 +7,9 @@ console.log(cable);
 let query=document.querySelector("#content");
 console.log(query);
 let queries=document.querySelector("p");
-console.log(queries);
+console.log(queries);*/
+let value1=document.getElementById("wish");
+value1.innerHTML="<h1><i><u>Happy Birthday to you🥳!</u></i></h1>";
+value1.style.color="blue";
+value1.style.fontSize="40px";
+value1.style.fontStyle="Arial, sans-serif";
