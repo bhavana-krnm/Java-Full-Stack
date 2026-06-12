bd60efeb97cff278d57590e88e07e4940b0ba4ce
bhavana-krnm <bhavanakrnm@gmail.com>
2026-06-12 11:52:09 +0530

    css layouts

--- manipulation.js
+++ manipulation.js
@@ -1,2 +1,10 @@
-const greet = document.getElementById('wish');
- console.log(greet.innerText);
+let pen=document.getElementById("wish");
+console.log(pen);
+let mouse=document.getElementsByClassName("wishes");
+console.log(mouse);
+let cable=document.getElementsByTagName("p");
+console.log(cable);
+let query=document.querySelector("#content");
+console.log(query);
+let queries=document.querySelector("p");
+console.log(queries);
